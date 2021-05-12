e0799d19b1166d99c7d17a3dbc9e15c3bf6cd124
steppi <albert_steppi@hms.harvard.edu>
2021-05-12 13:41:50 -0400

    Add prediction capabilities to BetaRegressor

--- opaque/beta_regression.py
+++ opaque/beta_regression.py
@@ -1,8 +1,10 @@
+import numpy as np
 import pymc3 as pm
-from sklearn.base import BaseEstimator
+from sklearn.base import BaseEstimator, RegressorMixin
+from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 
 
-class BetaRegressor(BaseEstimator):
+class BetaRegressor(BaseEstimator, RegressorMixin):
     def __init__(self, coefficient_prior_type='normal',
                  coefficient_prior_scale=100.0,
                  intercept_prior_type='normal',
@@ -13,10 +15,15 @@ class BetaRegressor(BaseEstimator):
         self.intercept_prior_type = intercept_prior_type
         self.intercept_prior_scale = intercept_prior_scale
         self.sampler_args = sampler_args
+        self.mean_use_cols = None
+        self.disp_use_cols = None
         self.model_ = None
         self.trace_ = None
 
     def fit(self, X, y, mean_use_cols=None, disp_use_cols=None):
+        self.mean_use_cols = mean_use_cols
+        self.disp_use_cols = disp_use_cols
+        X, y = check_X_y(X, y)
         with pm.Model() as model:
             if self.intercept_prior_type == 'normal':
                 int_mean = pm.Normal('int_mean', 0.0,
@@ -35,6 +42,9 @@ class BetaRegressor(BaseEstimator):
             X_disp = X[:] if disp_use_cols is None else X[:, disp_use_cols]
             ncols_mean = X_mean.shape[1]
             ncols_disp = X_disp.shape[1]
+            X_mean_obs = pm.Data('X_mean_obs', X_mean)
+            X_disp_obs = pm.Data('X_disp_obs', X_disp)
+            y_obs = pm.Data('y_obs', y)
             if self.coefficient_prior_type == 'normal':
                 beta_mean = pm.Normal('beta_mean', 0.0,
                                       self.coefficient_prior_scale,
@@ -52,11 +62,50 @@ class BetaRegressor(BaseEstimator):
             else:
                 ValueError('coefficient_prior_type must be one of "normal" '
                            'or "laplace"')
-            mu_est = pm.math.invlogit(int_mean +
-                                      pm.math.dot(X_mean, beta_mean))
-            nu_est = pm.math.exp(int_disp + pm.math.dot(X_disp, beta_disp))
+            mu_est = pm.\
+                Deterministic('mu_est',
+                              pm.math.invlogit(int_mean +
+                                               pm.math.dot(X_mean_obs,
+                                                           beta_mean)))
+            nu_est =pm.\
+                Deterministic('nu_est',
+                              pm.math.exp(int_disp +
+                                           pm.math.dot(X_disp_obs, beta_disp)))
             y_est = pm.Beta('y_est', alpha=mu_est * nu_est,
-                            beta = (1 - mu_est) * nu_est, observed=y)
+                            beta = (1 - mu_est) * nu_est, observed=y_obs)
             trace = pm.sample(**self.sampler_args)
             self.trace_ = trace
             self.model_ = model
+
+    def get_posterior_predictions(self, X, **pymc3_args):
+        check_is_fitted(self)
+        if 'var_names' in pymc3_args:
+            var_names = pymc3_args.pop('var_names')
+        else:
+            var_names = ['y_est', 'mu_est', 'nu_est']
+        mean_use_cols, disp_use_cols = self.mean_use_cols, self.disp_use_cols
+        X = check_array(X)
+        X_mean = X[:] if mean_use_cols is None else X[:, mean_use_cols]
+        X_disp = X[:] if disp_use_cols is None else X[:, disp_use_cols]
+        with self.model_:
+            pm.set_data({'X_mean_obs': X_mean,
+                         'X_disp_obs': X_disp,
+                         'y_obs': np.empty(len(X_mean))})
+            post_pred = pm.\
+                fast_sample_posterior_predictive(self.trace_,
+                                                 var_names=var_names,
+                                                 **pymc3_args)
+        
+        return post_pred
+    
+    def predict(self, X):
+        post_pred = self.get_posterior_predictions(X, var_names=['y_est'])
+        return np.mean(post_pred['y_est'], axis=0)
+
+    def predict_shape_params(self, X):
+        post_pred = self.get_posterior_predictions(X)
+        mu = np.mean(post_pred['mu_est'] ,axis=0)
+        nu = np.mean(post_pred['nu_est'], axis=0)
+        alpha = mu * nu
+        beta = (1 - mu) * nu
+        return np.vstack([alpha, beta]).T
