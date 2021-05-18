26b064e63ba602afc4480dd2e9ad5a64cb45906b
steppi <albert_steppi@hms.harvard.edu>
2021-05-18 14:53:32 -0400

    Add pipeline that can call any method of final est

--- opaque/ood/forest_svm.py
+++ opaque/ood/forest_svm.py
@@ -1,8 +1,21 @@
 import numpy as np
 from sklearn.svm import OneClassSVM
+from sklearn.pipeline import Pipeline
 from sklearn.base import BaseEstimator
+from sklearn.utils.metaestimators import if_delegate_has_method
 
 from opaque.ood._tree_kernel import tree_kernel
+
+
+class AnyMethodPipeline(Pipeline):
+    """sklearn Pipeline that allows any method of final estimator to be called
+    """
+    @if_delegate_has_method(delegate='_final_estimator')
+    def apply_method(self, method, X, **params):
+        Xt = X
+        for _, name, transform in self._iter(with_final=False):
+            Xt = transform.transform(Xt)
+        return getattr(self.steps[-1][-1], method)(X, **params)
 
 
 def make_forest_kernel(trained_forest):
