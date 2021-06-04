28c2a3863a492c211e86fd2789466bb010b89e01
steppi <albert_steppi@hms.harvard.edu>
2021-06-04 12:58:30 -0400

    Make OneClassSVM wrapper work with any kernel

--- opaque/ood/svm.py
+++ opaque/ood/svm.py
@@ -5,29 +5,11 @@ from sklearn.utils.validation import check_is_fitted
 from opaque.ood.utils import load_array, serialize_array
 
 
-class LinearOneClassSVM(OneClassSVM):
-    def __init__(
-        self,
-        nu=0.5,
-        tol=0.001,
-        shrinking=True,
-        cache_size=200,
-        verbose=False,
-        max_iter=-1,
-    ):
-        super().__init__(
-            kernel="linear",
-            nu=nu,
-            tol=tol,
-            shrinking=shrinking,
-            cache_size=cache_size,
-            verbose=False,
-            max_iter=-1,
-        )
-
+class SerializableOneClassSVM(OneClassSVM):
     def get_model_info(self):
         check_is_fitted(self)
         return {
+            "gamma": self._gamma,
             "sparse": self._sparse,
             "params": self.get_params(),
             "shape_fit": list(self.shape_fit_),
@@ -40,23 +22,28 @@ class LinearOneClassSVM(OneClassSVM):
 
     def feature_scores(self):
         check_is_fitted(self)
-        return self.coef_
+        try:
+            return self.coef_
+        except AttributeError:
+            raise AttributeError(
+                "Feature scores only available when using a linear kernel."
+            )
 
     @classmethod
     def load_model_info(cls, model_info):
-        model = LinearOneClassSVM(**model_info["params"])
+        model = SerializableOneClassSVM(**model_info["params"])
+        model.fit_status_ = 0.0
+        model._gamma = model_info["gamma"]
         model._sparse = model_info["sparse"]
         model.shape_fit_ = tuple(model_info["shape_fit"])
+        model.support_ = load_array(model_info["support"])
         model.intercept_ = load_array(model_info["intercept"])
         model.dual_coef_ = load_array(model_info["dual_coef"])
-        model.support_vectors_ = load_array(model_info["support_vectors"])
-        model.support_ = load_array(model_info["support"])
         model._n_support = load_array(model_info["n_support"])
-        model.fit_status_ = 0.0
-        model._intercept_ = model.intercept_
-        model._dual_coef_ = model.dual_coef_
-        model._gamma = 0.0
+        model.support_vectors_ = load_array(model_info["support_vectors"])
         model.offset_ = -model.intercept_
         model.class_weight_ = np.array([])
+        model._intercept_ = model.intercept_
+        model._dual_coef_ = model.dual_coef_
         model._probA, model._probB = np.array([]), np.array([])
         return model
