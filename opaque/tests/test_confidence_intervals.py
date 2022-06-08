2fa0f856c31ef79080e056b0ca0171518231cc06
steppi <albert_steppi@hms.harvard.edu>
2022-06-07 21:50:25 -0400

    Set seeds in confidence interval tests
    - also set random_state

--- opaque/tests/test_confidence_intervals.py
+++ opaque/tests/test_confidence_intervals.py
@@ -17,15 +17,18 @@ def test_equal_tailed_interval(test_input):
     n_trials = 100
     theta, sens_a, sens_b, spec_a, spec_b, sample_size = test_input
     hits = 0
+    random_state = np.random.RandomState(561)
     for _ in range(n_trials):
         sensitivity = beta.rvs(sens_a, sens_b)
         specificity = beta.rvs(spec_a, spec_b)
         n, t, _, _, _ = run_trial_for_theta(
-            theta,
-            sensitivity,
-            specificity,
-            sample_size,
-            np.random.RandomState()
+            (
+                theta,
+                sensitivity,
+                specificity,
+                sample_size,
+                random_state,
+            ),
         )
         interval = equal_tailed_interval(
             n, t, sens_a, sens_b, spec_a, spec_b, alpha=0.1
@@ -43,15 +46,18 @@ def test_highest_density_interval(test_input):
     n_trials = 100
     theta, sens_a, sens_b, spec_a, spec_b, sample_size = test_input
     hits = 0
+    random_state = np.random.RandomState(1105)
     for _ in range(n_trials):
         sensitivity = beta.rvs(sens_a, sens_b)
         specificity = beta.rvs(spec_a, spec_b)
         n, t, _, _, _ = run_trial_for_theta(
-            theta,
-            sensitivity,
-            specificity,
-            sample_size,
-            np.random.RandomState()
+            (
+                theta,
+                sensitivity,
+                specificity,
+                sample_size,
+                random_state,
+            ),
         )
         interval = highest_density_interval(
             n, t, sens_a, sens_b, spec_a, spec_b, alpha=0.1
