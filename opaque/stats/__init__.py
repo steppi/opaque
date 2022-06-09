68f177a57ee3e775f9f54db3315c8c7ef9d734fc
steppi <albert_steppi@hms.harvard.edu>
2022-06-09 05:57:42 -0400

    Update __init__

--- opaque/stats/__init__.py
+++ opaque/stats/__init__.py
@@ -5,8 +5,6 @@ from opaque.stats._stats import (
     equal_tailed_interval,
     highest_density_interval,
     log_betainc_wrapper as log_betainc,
-    prevalence_cdf_positive_fixed_wrapper as prevalence_cdf_positive_fixed,
-    prevalence_cdf_negative_fixed_wrapper as prevalence_cdf_negative_fixed,
     prevalence_cdf_fixed_wrapper as prevalence_cdf_fixed,
     prevalence_cdf_wrapper as prevalence_cdf,
 )
