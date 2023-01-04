7a00cf452b90393e69502bf4d61a0eace0a315c0
steppi <albert_steppi@hms.harvard.edu>
2023-01-04 01:02:08 -0500

    Make the setup actually work

--- setup.py
+++ setup.py
@@ -2,13 +2,8 @@ import numpy as np
 from os import path
 from Cython.Build import cythonize
 from setuptools.extension import Extension
-from setuptools import setup, find_packages
+from setuptools import setup
 
-
-here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, "README.md"), encoding="utf-8") as f:
-    long_description = f.read()
 
 ext = ".pyx"
 
@@ -39,31 +34,5 @@ extensions = cythonize(
     compiler_directives={"language_level": 3},
 )
 
-setup(
-    name="opaque",
-    version="0.0.0",
-    description="Outlier prevalence analysis for quantification"
-    " of unknown entities.",
-    author="opaque developers, Harvard Medical School",
-    author_email="albert.steppi@gmail.com",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-    ],
-    packages=find_packages(),
-    install_requires=[
-        "appdirs",
-        "liblinear-official",
-        "cython",
-        "numba",
-        "pymc",
-        "scikit-learn",
-        "statsmodels",
-    ],
-    extras_require={"test": ["mpmath", "pandas", "pytest", "pytest-cov"]},
-    ext_modules=extensions,
-    include_package_data=True,
-)
+setup(ext_modules=extensions)
+
