01862897c06bd555ed7b942574a873e78b4fd517
steppi <albert_steppi@hms.harvard.edu>
2023-01-05 17:59:28 -0500

    Add comments for datasplits.py

--- scripts/train_betabinom/datasplits.py
+++ scripts/train_betabinom/datasplits.py
@@ -1,3 +1,21 @@
+"""Helps split data in a way that avoids information leak
+
+In each split, we should never have a pair of datapoints A, B from train and
+test respectively for which
+
+1) A and B have the same grounding
+
+or
+
+2) A and B have the same agent text
+
+If A and B have the same grounding, then their respective anomaly detectors
+will use the same exact training data. If A and B have the same agent text,
+then there will be a large overlap in their validation data, since the
+validation data will have come from the same adeft model.
+"""
+
+
 import networkx as nx
 import pandas as pd
 from sklearn.model_selection import GroupShuffleSplit
@@ -8,6 +26,9 @@ import opaque.locations as loc
 if __name__ == "__main__":
     df = pd.read_csv(loc.ADEFT_BETABINOM_DATASET_PATH, sep=',')
 
+    # We're creating a graph structure on the datapoints in such a way that
+    # two points are linked if they have either the same grounding or the
+    # same agent text.
     G = nx.Graph()
     G.add_nodes_from(df.index)
     for i in range(len(df)):
@@ -18,8 +39,14 @@ if __name__ == "__main__":
                     row1.grounding == row2.grounding
             ):
                 G.add_edge(i, j)
+    # We find the connected components in this graph. Any two points in the
+    # same connected component are considered to be in the same group. We can
+    # then use group split methods to ensure that no train/test split has points
+    # from the same group in both train and test.
     components = nx.connected_components(G)
     components = [frozenset(component) for component in components]
+
+    # There's some bookkeeping involved.
     component_to_group = {
         component: i for i, component in enumerate(components)
     }
