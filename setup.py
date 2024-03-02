dcabc4032b5eac36e915923d5580dd2ebc4f5aac
Albert Steppi <albert.steppi@gmail.com>
2024-03-01 19:16:15 -0500

    Chuck out the random forest kernel

--- setup.py
+++ setup.py
@@ -4,10 +4,8 @@ from Cython.Build import cythonize
 from setuptools.extension import Extension
 from setuptools import setup
 
-
 ext = ".pyx"
 
-defs = [("NPY_NO_DEPRECATED_API", 0)]
 inc_path = np.get_include()
 
 extensions = [
@@ -15,12 +13,6 @@ extensions = [
         "opaque.stats._stats",
         ["src/opaque/stats/_stats" + ext],
         include_dirs=[inc_path],
-    ),
-    Extension(
-        "opaque.ood._tree_kernel",
-        ["src/opaque/ood/_tree_kernel" + ext],
-        include_dirs=[inc_path],
-        define_macros=defs,
     ),
 ]
 
