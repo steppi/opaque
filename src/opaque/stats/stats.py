8ab1200404d22864113047ce63a8f2bc74cefaf5
steppi <1953382+steppi@users.noreply.github.com>
2026-01-06 13:44:02 -0500

    Get rid of a bunch of unnecessary stuff in stats.py

--- src/opaque/stats/stats.py
+++ src/opaque/stats/stats.py
@@ -34,68 +34,6 @@ def _round_interval(left, right, *, digits=6):
     return np.clip(left, 0.0, 1.0), np.clip(right, 0.0, 1.0)
 
 
-def true_positives(
-        y_true: ArrayLike, y_pred: ArrayLike, *, pos_label: Any = 1
-) -> float:
-    y_true, y_pred = column_or_1d(y_true), column_or_1d(y_pred)
-    return np.sum((y_true == pos_label) & (y_pred == pos_label))
-
-
-def true_negatives(
-        y_true: ArrayLike, y_pred: ArrayLike, *, pos_label: Any = 1
-) -> float:
-    y_true, y_pred = column_or_1d(y_true), column_or_1d(y_pred)
-    return np.sum((y_true != pos_label) & (y_pred != pos_label))
-
-
-def false_positives(
-        y_true: ArrayLike, y_pred: ArrayLike, *, pos_label: Any = 1
-) -> float:
-    y_true, y_pred = column_or_1d(y_true), column_or_1d(y_pred)
-    return np.sum((y_true != pos_label) & (y_pred == pos_label))
-
-
-def false_negatives(
-        y_true: ArrayLike, y_pred: ArrayLike, *, pos_label: Any = 1
-) -> float:
-    y_true, y_pred = column_or_1d(y_true), column_or_1d(y_pred)
-    return np.sum((y_true == pos_label) & (y_pred != pos_label))
-
-
-def sensitivity_score(
-        y_true: ArrayLike, y_pred: ArrayLike, *, pos_label: Any = 1
-) -> float:
-    tp = true_positives(y_true, y_pred, pos_label)
-    fn = false_negatives(y_true, y_pred, pos_label)
-    try:
-        result = tp / (tp + fn)
-    except ZeroDivisionError:
-        logger.warning("No positive examples in sample. Returning 0.0")
-        result = 0.0
-    return result
-
-
-def specificity_score(
-        y_true: ArrayLike, y_pred: ArrayLike, *, pos_label: Any = 1
-) -> float:
-    tn = true_negatives(y_true, y_pred, pos_label)
-    fp = false_positives(y_true, y_pred, pos_label)
-    try:
-        result = tn / (tn + fp)
-    except ZeroDivisionError:
-        logger.warning("No negative examples in sample. Returning 0.0")
-        result = 0.0
-    return result
-
-
-def youdens_j_score(
-        y_true: ArrayLike, y_pred: ArrayLike, *, pos_label: Any = 1
-) -> float:
-    sens = sensitivity_score(y_true, y_pred, pos_label)
-    spec = specificity_score(y_true, y_pred, pos_label)
-    return sens + spec - 1
-
-
 def KL_beta(a1, b1, a2, b2):
     """Compute KL Divergence between two beta distributions."""
     output = sc.betaln(a1, b1) - sc.betaln(a2, b2)
@@ -104,42 +42,10 @@ def KL_beta(a1, b1, a2, b2):
     return output
 
 
-def NKLD(p_true, p_pred):
-    """Compute Normalized Kullback-Leibler Divergence Metric."""
-    KL = (
-        p_true * np.log(p_true / p_pred)
-        + (1 - p_true) * np.log((1 - p_true) / (1 - p_pred))
-    )
-    return np.mean(2 * sc.expit(KL) - 1)
-
-
 def log_score_betabinom(k_true, n_true, a_pred, b_pred):
+    """Compute log score from betabinomial likelihood."""
     vals = stats.betabinom.logpmf(k_true, n_true, a_pred, b_pred)
     return np.mean(np.where(vals > 0, -np.inf, vals))
-
-
-def simple_prevalence_interval(
-        n: int,
-        t: int,
-        sens: float,
-        spec: float,
-        *,
-        alpha: float = 0.1,
-        method: str = "beta",
-) -> tuple[float,float]:
-    """Compute simple prevalence interval based on linear transform.
-
-    phi = sens * theta + (1 - spec) * (1 - theta)
-
-    Where phi is proportion of positive tests and theta is unknown
-    prevalence value.
-    """
-    a, b = proportion_confint(t, n, alpha=alpha, method=method)
-    J = sens + spec - 1
-    fnr = 1 - spec
-    c = min(max(0, (a - fnr) / J), 1)
-    d = max(min(1, (b - fnr) / J), 0)
-    return c, d
 
 
 def sample_prevalence_posterior(
