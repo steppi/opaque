92f3e454537c6ed93f6d38090fe53203cae97cb3
steppi <1953382+steppi@users.noreply.github.com>
2026-01-11 12:55:52 -0500

    fixup with previous

--- src/opaque/stats/stats.py
+++ src/opaque/stats/stats.py
@@ -405,6 +405,4 @@ class HighestDensityRegion2d:
         ax.grid(True, which='major', linestyle='-', linewidth=0.8, alpha=0.6)
         ax.grid(True, which='minor', linestyle=':', linewidth=0.5, alpha=0.4)
 
-        ax.set_axisbelow(False)
-
         return fig, ax
